3e8947ffffca05827ae40c3af6cfb67321bf3364
Kent Rancourt <kent.rancourt@gmail.com>
2020-04-15 17:49:16 -0400

    just adding a quick todo comment

--- container_config.go
+++ container_config.go
@@ -4,4 +4,5 @@ type ContainerConfig struct {
 	Image           string `json:"image" bson:"image"`
 	ImagePullPolicy string `json:"imagePullPolicy" bson:"imagePullPolicy"`
 	Command         string `json:"command" bson:"command"`
+	// TODO: Should be able to add non-sensitive environment variables here
 }
