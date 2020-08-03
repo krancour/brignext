251f675d2dfcfc58811d302572867245fc69d191
Kent Rancourt <kent.rancourt@gmail.com>
2020-08-03 16:47:11 -0400

    more docs

--- apiserver/internal/sdk/meta/meta.go
+++ apiserver/internal/sdk/meta/meta.go
@@ -1,27 +1,48 @@
 package meta
 
-import (
-	"time"
-)
+import "time"
 
+// APIVersion represents the API and major version thereof with which this
+// version of the BrigNext API server is compatible.
 const APIVersion = "github.com/krancour/brignext/v2"
 
+// TypeMeta represents metadata about a resource type to help clients and
+// servers mutually head off potential confusion over types (and versions of
+// thereof) sent over the wire.
 type TypeMeta struct {
-	Kind       string `json:"kind,omitempty"`
+	// Kind specifies the type of a serialized resource.
+	Kind string `json:"kind,omitempty"`
+	// APIVersion specifiesthe major version of the BrigNext API with which the
+	// client or server having serialized the resource is compatible.
 	APIVersion string `json:"apiVersion,omitempty"`
 }
 
-// nolint: lll
+// ObjectMeta represents metadata about an instance of a resource. The fields
+// of this type are broadly applicable to most if not all resource types.
 type ObjectMeta struct {
+	// ID is an immutable resource identifier.
 	ID string `json:"id,omitempty" bson:"id,omitempty"`
-	// The JSON schema doesn't permit the fields below to be set via the API.
+	// Created indicates the time at which a resource was created.
 	Created *time.Time `json:"created,omitempty" bson:"created,omitempty"`
-	// CreatedBy   string     `json:"createdBy,omitempty" bson:"createdBy,omitempty"`
-	LastUpdated *time.Time `json:"lastUpdated,omitempty" bson:"lastUpdated,omitempty"`
-	// LastUpdatedBy string     `json:"lastUpdatedBy,omitempty" bson:"lastUpdatedBy,omitempty"`
+	// CreatedBy indicates who or what created the resource.
+	CreatedBy *PrincipalReference `json:"createdBy,omitempty" bson:"createdBy,omitempty"`
+	// LastUpdated indicates the time at which a resource was last updated.
+	LastUpdated *time.Time `json:"lastUpdated,omitempty" bson:"lastUpdated,omitempty"` // nolint: lll
+	// LastUpdatedBy indicates who or what last updated the resource.
+	LastUpdatedBy *PrincipalReference `json:"lastUpdatedBy,omitempty" bson:"lastUpdatedBy,omitempty"`
 }
 
-// nolint: lll
+// PrincipalReference is a reference to a user of the system-- either a human
+// user or service account.
+type PrincipalReference struct {
+	// User references a human User by their ID.
+	User string `json:"user,omitempty" bson:"user,omitempty"`
+	// ServiceAccount references a ServiceAccount by its ID.
+	ServiceAccount string `json:"serviceAcount,omitempty" bson:"serviceAcount,omitempty"` // nolint: lll
+}
+
+// ObjectReferenceMeta is an abridged representation of resource metadata used
+// by other abridged representations of various resource types.
 type ObjectReferenceMeta struct {
 	ID      string    `json:"id,omitempty" bson:"id"`
 	Created time.Time `json:"created,omitempty" bson:"created"`
--- sdk/meta/meta.go
+++ sdk/meta/meta.go
@@ -1,28 +1,56 @@
 package meta
 
-import (
-	"time"
-)
+import "time"
 
+// APIVersion represents the API and major version thereof with which this
+// version of the BrigNext SDK is compatible.
 const APIVersion = "github.com/krancour/brignext/v2"
 
+// TypeMeta represents metadata about a resource type to help clients and
+// servers mutually head off potential confusion over types (and versions of
+// thereof) sent over the wire.
 type TypeMeta struct {
-	Kind       string `json:"kind,omitempty"`
+	// Kind specifies the type of a serialized resource.
+	Kind string `json:"kind,omitempty"`
+	// APIVersion specifiesthe major version of the BrigNext API with which the
+	// client or server having serialized the resource is compatible.
 	APIVersion string `json:"apiVersion,omitempty"`
 }
 
-// nolint: lll
+// ObjectMeta represents metadata about an instance of a resource. The fields
+// of this type are broadly applicable to most if not all resource types.
 type ObjectMeta struct {
+	// ID is an immutable resource identifier.
 	ID string `json:"id,omitempty"`
-	// The JSON schema doesn't permit the fields below to be set via the API.
-	// Pointers must be nil and strings must be empty when outbound.
+	// Created indicates the time at which a resource was created. This is
+	// recorded by the system. Clients must leave the value of this field set to
+	// nil when using the API to create or update resources.
 	Created *time.Time `json:"created,omitempty"`
-	// CreatedBy   string     `json:"createdBy,omitempty"`
+	// CreatedBy indicates who or what created the resource. This is recorded by
+	// the system. Clients must leave the value of this field set to nil when
+	// using the API to create or update resources.
+	CreatedBy *PrincipalReference `json:"createdBy,omitempty"`
+	// LastUpdated indicates the time at which a resource was last updated. This
+	// is recorded by the system. Clients must leave the value of this field set
+	// to nil when using the API to create or update resources.
 	LastUpdated *time.Time `json:"lastUpdated,omitempty"`
-	// LastUpdatedBy string     `json:"lastUpdatedBy,omitempty"`
+	// LastUpdatedBy indicates who or what last updated the resource. This is
+	// recorded by the system. Clients must leave the value of this field set to
+	// nil when using the API to create or update resources.
+	LastUpdatedBy *PrincipalReference `json:"lastUpdatedBy,omitempty"`
 }
 
-// nolint: lll
+// PrincipalReference is a reference to a user of the system-- either a human
+// user or service account.
+type PrincipalReference struct {
+	// User references a human User by their ID.
+	User string `json:"user,omitempty"`
+	// ServiceAccount references a ServiceAccount by its ID.
+	ServiceAccount string `json:"serviceAcount,omitempty"`
+}
+
+// ObjectReferenceMeta is an abridged representation of resource metadata used
+// by other abridged representations of various resource types.
 type ObjectReferenceMeta struct {
 	ID      string    `json:"id,omitempty"`
 	Created time.Time `json:"created,omitempty"`
