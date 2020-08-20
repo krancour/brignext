31a2056fe1bc999228a7dced4d702b5fbbd5b34f
Kent Rancourt <kent.rancourt@gmail.com>
2020-08-20 14:17:28 -0400

    remove comment that's no longer applicable

--- worker/src/brigadier/projects.ts
+++ worker/src/brigadier/projects.ts
@@ -1,7 +1,5 @@
 export interface Project {
   id: string
-  // TODO: The kubernetes field can go away when we transition to using the API
-  // to watch job status
   kubernetes: ProjectKubernetesConfig
   secrets: Map<string, string>
 }
