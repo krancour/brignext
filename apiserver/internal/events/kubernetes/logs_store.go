048681b7e168ddb061c8568222908e32381a72bb
Kent Rancourt <kent.rancourt@gmail.com>
2020-08-20 12:36:30 -0400

    make streaming from warm log storage work

--- apiserver/internal/events/kubernetes/logs_store.go
+++ apiserver/internal/events/kubernetes/logs_store.go
@@ -25,6 +25,7 @@ func NewLogsStore(kubeClient *kubernetes.Clientset) events.LogsStore {
 	}
 }
 
+// TODO: DRY this up
 func (l *logsStore) GetLogs(
 	ctx context.Context,
 	event brignext.Event,
@@ -86,7 +87,7 @@ func (l *logsStore) GetLogs(
 	return logEntries, nil
 }
 
-// TODO: Implement this
+// TODO: DRY this up
 func (l *logsStore) StreamLogs(
 	ctx context.Context,
 	event brignext.Event,
@@ -104,7 +105,6 @@ func (l *logsStore) StreamLogs(
 		&v1.PodLogOptions{
 			Container:  opts.Container,
 			Timestamps: true,
-			Follow:     true,
 		},
 	)
 	podLogs, err := req.Stream(ctx)
@@ -116,7 +116,52 @@ func (l *logsStore) StreamLogs(
 			event.Kubernetes.Namespace,
 		)
 	}
-	defer podLogs.Close()
 
-	return nil, nil
+	logEntryCh := make(chan brignext.LogEntry)
+
+	go func() {
+		defer podLogs.Close()
+		defer close(logEntryCh)
+		buffer := bufio.NewReader(podLogs)
+		for {
+			logEntry := brignext.LogEntry{}
+			logLine, err := buffer.ReadString('\n')
+			if err == io.EOF {
+				break
+			}
+			// The last character should be a newline that we don't want, so let's
+			// remove that
+			logLine = logLine[:len(logLine)-1]
+			logLineParts := strings.SplitN(logLine, " ", 2)
+			if len(logLineParts) == 2 {
+				timeStr := logLineParts[0]
+				t, err := time.Parse(time.RFC3339, timeStr)
+				if err == nil {
+					logEntry.Time = &t
+				}
+				logEntry.Message = logLineParts[1]
+			} else {
+				logEntry.Message = logLine
+			}
+			select {
+			case logEntryCh <- logEntry:
+			case <-ctx.Done():
+				return
+			}
+		}
+		podLogs.Close()
+		// We let this goroutine hang until the context times out or is canceled.
+		// Why? When logs are retrieved from COLD storage (e.g. MongoDB) we never
+		// know whether the log aggregator has stored all the logs we're trying to
+		// stream, so we don't disconnect since there's a possibility there is more
+		// coming. We leave it up to the client to decide to disconnect. For
+		// consistency, we're leaving it up to the client to disconnect here as
+		// well. We can revisit this if we can make the COLD log storage smarter
+		// about knowing when it has reached the end of a stream, in which case both
+		// warm and cold storage could both disconnect when the end of a stream is
+		// reached and they would still be consistent with one another.
+		<-ctx.Done()
+	}()
+
+	return logEntryCh, nil
 }
